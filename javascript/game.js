4aa2f837b3a88f3cabcbc814997e87cd1fc06e07
alireza mohammadkhani <alirezamohammadkhani2@gmail.com>
2021-12-29 01:40:13 +0330

    ad random number

--- javascript/game.js
+++ javascript/game.js
@@ -1,9 +1,12 @@
 let yourChoose = document.querySelectorAll('.yourChoose div');
+let pcElement = document.querySelectorAll('.pcChoose div');
+
+let randomNumber , pcChoose ;
 yourChoose[0].addEventListener('click' , choose);
 yourChoose[1].addEventListener('click' , choose);
 yourChoose[2].addEventListener('click' , choose);
 function choose() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
-    
+    randomNumber = Math.floor(Math.random() * 3);
+    pcChoose = pcElement[randomNumber];
+    pcChoose.classList.remove('display');
 }
